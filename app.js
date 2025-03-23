72be6dc096bb4484a1bb78aa77a56043fe22276f
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-03-23 11:25:55 -0700

    feat: integrate MongoDB connection in app startup
    - Imported `connectToDatabase` into `app.js`
    - Established DB connection inside server listen callback

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import routes from "./routes/index.js";
+import connectToDatabase from "./database/mongodb.js";
 import { PORT } from "./config/env.js";
 
 const app = express();
 
 app.use("/api/v1", routes);
 
-app.listen(PORT);
+app.listen(PORT, async () => {
+  console.log(`Listening to port: ${PORT}`);
+
+  await connectToDatabase();
+});
 
 export default app;
