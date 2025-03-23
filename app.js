2afbca541c5b26f2b888d1bf6676cd611131152f
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-03-23 13:15:21 -0700

    Add middleware setup to app.js: error handling, body parsing, and cookie parser

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from "express";
 import routes from "./routes/index.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from ".//middlewares/error.middleware.js";
 import { PORT } from "./config/env.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use("/api/v1", routes);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`Listening to port: ${PORT}`);
