22ba28f9283d73992776571c88110a4c41148d60
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-03-24 10:18:04 -0700

    refactor:

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 import express from "express";
-import routes from "./routes/index.js";
-import connectToDatabase from "./database/mongodb.js";
-import errorMiddleware from ".//middlewares/error.middleware.js";
-import rateLimiter from "./middlewares/rateLimiter.middleware.js";
-import { PORT } from "./config/env.js";
-import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import hpp from "hpp";
 import xssClean from "xss-clean";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import routes from "./routes/index.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import rateLimiter from "./middlewares/rateLimiter.middleware.js";
+import { PORT } from "./config/env.js";
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(rateLimiter);
+// Security & Middleware
 app.use(helmet());
 app.use(hpp());
 app.use(xssClean());
 app.use(cors());
+app.use(rateLimiter);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// Routes
 app.use("/api/v1", routes);
 
+// Error Handler
 app.use(errorMiddleware);
 
-app.listen(PORT, async () => {
-  console.log(`Listening to port: ${PORT}`);
-
+// Server Start
+const startServer = async () => {
   await connectToDatabase();
-});
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
 
 export default app;
